3c2244afff761382286c5e76c4bdcfda0ae9ddf4
yaskovdev <yaskovdev@gmail.com>
2023-04-30 15:04:25 +0300

    Fix union deserialization.

--- BlockheadGameBackEnd/MoveRequest.fs
+++ BlockheadGameBackEnd/MoveRequest.fs
@@ -1,26 +1,23 @@
 namespace BlockheadGameBackEnd
 
-open System.ComponentModel
+open System.Text.Json.Serialization
 
-type DifficultyTypeConverter() =
-    inherit TypeConverter()
-
-    override this.CanConvertFrom(context, sourceType) = true
-    override this.CanConvertTo(context, destinationType) = true
-
-    override this.ConvertFrom(context, culture, value) =
-        match value with
-        | :? string as res -> Util.fromString res
-        | _ -> base.ConvertFrom(context, culture, value)
-
-    override this.ConvertTo(context, culture, value, destinationType) = Util.toString value
-
-type [<TypeConverter(typeof<DifficultyTypeConverter>)>] Difficulty =
+type Difficulty =
     | Easy
     | Medium
     | Hard
 
+type DifficultyTypeConverter() =
+    inherit JsonConverter<Difficulty>()
+
+    override this.Read(reader, _, _) =
+        (Util.fromString<Difficulty> (reader.GetString())).Value
+
+    override this.Write(writer, value, _) =
+        writer.WriteStringValue(Util.toString value)
+
 type MoveRequest =
     { Field: seq<string>
       UsedWords: seq<string>
+      [<JsonConverter(typeof<DifficultyTypeConverter>)>]
       Difficulty: Difficulty }
