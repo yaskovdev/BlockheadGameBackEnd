d8d2d9e9d82c98f115548da2973a73fe16acfde1
Sergei Iaskov <sergeiiaskov@microsoft.com>
2023-04-30 18:34:50 +0300

    Try to only use path, without maintaining a word (that can be restored from path using field). Not sure if this version is more maintainable, need to work with it a bit.

--- BlockheadGameBackEnd/Game.fs
+++ BlockheadGameBackEnd/Game.fs
@@ -11,18 +11,16 @@ type private Move = Field.Cell * char
 
 type private Path = seq<Field.Cell>
 
-type private WordPath = string * Path
-
 let private contains x = Seq.exists ((=) x)
+
+let private wordInPath (field: Field.Field) (path: Path) =
+    String(Seq.toArray (Seq.map (fun (x, y) -> if field[x][y] = '.' then raise (Exception("GOVNO")) else field[x][y]) path))
 
 let private wordPickRange (difficulty: Difficulty) : int =
     match difficulty with
     | Easy -> 30
     | Medium -> 13
     | Hard -> 0
-
-let private appendCell (field: Field.Field) ((word, path): WordPath) (x, y) : WordPath =
-    (word + Char.ToString(field[x][y]), Seq.append [ (x, y) ] path)
 
 let private alphabet = [ 'А' .. 'Е' ] @ [ 'Ё' ] @ [ 'Ж' .. 'Я' ]
 
@@ -35,35 +33,38 @@ let private reachableCells (field: Field.Field) (cell: Field.Cell) (visited: Set
 let private availableMoves (field: Field.Field) : seq<Move> =
     Seq.collect (fun cell -> (Seq.map (fun letter -> (cell, letter)) alphabet)) (Field.availableCells field)
 
-let rec private paths2 (prefixDictionary: Set<string>) (field: Field.Field) (current: Field.Cell) (visited: Set<Field.Cell>) ((word, path): WordPath): seq<WordPath> =
-    if Set.contains word prefixDictionary then Seq.append [(word, path)] (Seq.collect (fun cell -> paths2 prefixDictionary field cell (Set.add cell visited) (appendCell field (word, path) cell)) (reachableCells field current visited))
+let rec private paths2 (prefixDictionary: Set<string>) (field: Field.Field) (current: Field.Cell) (visited: Set<Field.Cell>) (path: Path): seq<Path> =
+    let word = (wordInPath field path)
+    if Set.contains word prefixDictionary then Seq.append [path] (Seq.collect (fun cell -> paths2 prefixDictionary field cell (Set.add cell visited) (Seq.append path [ cell ])) (reachableCells field current visited))
     else []
 
-let private paths (prefixDictionary: Set<string>) (field: Field.Field) (start: Field.Cell) : seq<WordPath> =
-    paths2 prefixDictionary field start (Set.ofSeq [ start ]) (Char.ToString(field[fst start][snd start]), [ start ])
+let private paths (prefixDictionary: Set<string>) (field: Field.Field) (start: Field.Cell) : seq<Path> =
+    paths2 prefixDictionary field start (Set.ofSeq [ start ]) [ start ]
 
-let private availableWords4 (prefixDictionary: Set<string>) (field: Field.Field) (updatedCell: Field.Cell): seq<WordPath> =
-    Seq.filter (fun (_, path) -> contains updatedCell path) (Seq.collect (paths prefixDictionary field) (Field.notEmptyCellsOf field))
+let private availableWords4 (prefixDictionary: Set<string>) (field: Field.Field) (updatedCell: Field.Cell): seq<Path> =
+    Seq.filter (contains updatedCell) (Seq.collect (paths prefixDictionary field) (Field.notEmptyCellsOf field))
 
-let private availableWords3 (prefixDictionary: Set<string>) (field: Field.Field) (cell: Field.Cell, letter: char): seq<string * Path * (Field.Cell * char)> =
+let private availableWords3 (prefixDictionary: Set<string>) (field: Field.Field) (cell: Field.Cell, letter: char): seq<Path * (Field.Cell * char)> =
     let fieldAfterMove = Field.replaceLetter field cell letter
-    Seq.map (fun (word, path) -> (word, path, (cell, letter))) (availableWords4 prefixDictionary fieldAfterMove cell)
+    Seq.map (fun path -> (path, (cell, letter))) (availableWords4 prefixDictionary fieldAfterMove cell)
 
-let availableWords2 (prefixDictionary: Set<string>) (field: Field.Field) (moves: seq<Move>) =
-    Seq.distinctBy (fun (_, word, _) -> word) (Seq.collect (availableWords3 prefixDictionary field) moves)
+let private availableWords2 (prefixDictionary: Set<string>) (field: Field.Field) (moves: seq<Move>) =
+    Seq.distinctBy (fun (path, (cell, letter)) -> wordInPath (Field.replaceLetter field cell letter) path) (Seq.collect (availableWords3 prefixDictionary field) moves)
 
 let private availableWords (prefixDictionary: Set<string>) (field: Field.Field) =
     availableWords2 prefixDictionary field (availableMoves field)
 
 let makeMove prefixDictionary dictionary difficulty usedWords field : bool * Field.Field * string * Path * ((int * int) * char) =
     let availableWords = availableWords prefixDictionary field
-    let foundWords = Seq.filter (fun (w, _, _) -> Set.contains w dictionary && not (Seq.contains w usedWords)) availableWords
+    let longWords = Seq.filter (fun (path, _) -> Seq.length path > 3) availableWords
+    let longestWordsFirst = Seq.toList (Seq.sortWith (fun (a, _) (b, _) -> (Seq.length b).CompareTo(Seq.length a)) availableWords)
+    let foundWords = Seq.filter (fun (path, (cell, letter)) -> Set.contains (wordInPath (Field.replaceLetter field cell letter) path) dictionary && not (Seq.contains (wordInPath (Field.replaceLetter field cell letter) path) usedWords)) availableWords
     if Seq.isEmpty foundWords then (false, field, "", [], ((0, 0), ' '))
     else
-        let longestWordsFirst = Seq.toList (Seq.sortWith (fun (_, a, _) (_, b, _) -> (Seq.length b).CompareTo(Seq.length a)) foundWords)
+        let longestWordsFirst = Seq.toList (Seq.sortWith (fun (a, _) (b, _) -> (Seq.length b).CompareTo(Seq.length a)) foundWords)
         let random = Random()
         let wordIndex = random.Next(Math.Min((wordPickRange difficulty) + 1, Seq.length longestWordsFirst))
         let oneOfLongestWord = longestWordsFirst[wordIndex]
-        let word, path, (cell, letter) = oneOfLongestWord
+        let path, (cell, letter) = oneOfLongestWord
         let updatedField = Field.replaceLetter field cell letter
-        (true, updatedField, word, path, (cell, letter))
+        (true, updatedField, (wordInPath updatedField path), path, (cell, letter))
